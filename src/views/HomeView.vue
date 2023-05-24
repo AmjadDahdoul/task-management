b5d93265c139117d4eaf463aff898efeb4ed05e5
Amjad Dahdoul <amjad.dahdoul@gmail.com>
2023-05-24 17:31:31 +0300

    init & Navbar

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container mx-auto">
+    <Navbar />
+  </div>
+</template>
+
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    Navbar,
+  },
+};
+</script>
